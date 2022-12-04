1bdf7e2a5e57e79858e7c9efee35d686f85aa033
Chris Swierczewski <cswiercz@gmail.com>
2022-12-04 19:02:56 +0000

    Day 02 [in-progress]

---
+++ src/bin/02.rs
@@ -0,0 +1,76 @@
+pub fn score(left: char, right: char) -> u32 {
+    let mut score = match right {
+        'X' => 1,
+        'Y' => 2,
+        'Z' => 3,
+        _ => 0,
+    };
+    score += match (left, right) {
+        ('A', 'Y') => 6,
+        ('B', 'Z') => 6,
+        ('C', 'X') => 6,
+        ('A', 'X') => 3,
+        ('B', 'Y') => 3,
+        ('C', 'Z') => 3,
+        _ => 0,
+    };
+    score
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let result = input
+        .split('\n')
+        .map(|line| {
+            let mut chars = line.split(' ');
+            score(chars.next().unwrap().chars().next().unwrap(), chars.next().unwrap().chars().next().unwrap())
+        })
+        .sum();
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let result = input
+        .split('\n')
+        .map(|line| {
+            let mut chars = line.split(' ');
+            let left = chars.next().unwrap().chars().next().unwrap();
+            let right = chars.next().unwrap().chars().next().unwrap();
+            match (left, right) {
+                ('A', 'X') => 3 + 0 ,
+                ('A', 'Y') => 2 + 3,
+                ('A', 'Z') => 1 + 6,
+                ('B', 'X') => 1 + 0,
+                ('B', 'Y') => 3 + 3,
+                ('B', 'Z') => 2 + 6,
+                ('C', 'X') => 2 + 0,
+                ('C', 'Y') => 1 + 3,
+                ('C', 'Z') => 3 + 6,
+                _ => 0,
+            }
+        })
+        .sum();
+    Some(result)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 2);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 2);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 2);
+        assert_eq!(part_two(&input), None);
+    }
+}
