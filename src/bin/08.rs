1e3b666e9f29de25e151571a8a529100368df148
Chris Swierczewski <chris.swierczewski@outreach.io>
2022-12-17 08:47:36 -0800

    Day 08 - Part 2 in progress and nasty

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,5 +1,5 @@
-use core::num;
-use std::collections::{HashMap, HashSet};
+use std::cmp::max;
+use std::collections::HashSet;
 
 pub fn get_grid(input: &str) -> Vec<Vec<u32>> {
     input
@@ -63,19 +63,86 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     }
 
-    /*
-    let mut visible: Vec<(usize, usize)> = visible.into_iter().collect();
-    visible.sort();
-    for (i, j) in visible.iter() {
-        println!("({}, {}) height = {}", i, j, grid[*i][*j]);
-    }
-    */
-
     Some(visible.len() as u32)
 }
 
+fn score(grid: &Vec<Vec<u32>>, i: usize, j: usize) -> u32 {
+    let nrows = grid.len();
+    let ncols = grid[0].len();
+
+    let mut pointer: usize;
+    let mut score: u32 = 1;
+    let mut count: u32;
+
+    // right
+    if j < ncols - 1 {
+        pointer = j + 1;
+        count = 1;
+        for idx in (j + 1)..ncols {
+            if grid[i][idx] > grid[i][pointer] {
+                count += 1;
+                pointer = idx;
+            }
+        }
+        score *= count;
+    }
+
+    // down
+    if i < nrows - 1 {
+        pointer = i + 1;
+        count = 1;
+        for idx in (i + 1)..nrows {
+            if grid[idx][j] > grid[pointer][j] {
+                count += 1;
+                pointer = idx;
+            }
+        }
+        score *= count;
+    }
+
+    // left
+    if j > 0 {
+        pointer = j - 1;
+        count = 1;
+        for idx in (0..j).rev() {
+            if grid[i][idx] > grid[i][pointer] {
+                count += 1;
+                pointer = idx;
+            }
+        }
+        score *= count;
+    }
+
+    // up
+    if i > 0 {
+        pointer = i - 1;
+        count = 1;
+        for idx in (0..i).rev() {
+            if grid[idx][j] > grid[pointer][j] {
+                count += 1;
+                pointer = idx;
+            }
+        }
+        score *= count;
+    }
+
+    score
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid = get_grid(input);
+    let nrows = grid.len();
+    let ncols = grid[0].len();
+
+    let mut best_score: u32 = 0;
+
+    for i in 0..nrows {
+        for j in 0..ncols {
+            best_score = max(best_score, score(&grid, i, j));
+        }
+    }
+
+    Some(best_score)
 }
 
 fn main() {
